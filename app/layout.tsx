f88c74b5492c74fe71e6b6dce94366526e8b1072
James McCann <144072603+JamesMcCannCoding@users.noreply.github.com>
2026-04-29 14:01:53 +1000

    Integrate Mailchimp script in layout
    Added Mailchimp script for site connection.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Building Beyond 2032",
   description: "Building Beyond 2032 website",
-
   icons: {
     icon: "/LogoDarkBackground.svg",
     shortcut: "/LogoDarkBackground.svg",
@@ -19,7 +19,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Script id="mailchimp-site-connection" strategy="afterInteractive">
+          {`
+            !function(c,h,i,m,p){
+              m=c.createElement(h),
+              p=c.getElementsByTagName(h)[0],
+              m.async=1,
+              m.src=i,
+              p.parentNode.insertBefore(m,p)
+            }(
+              document,
+              "script",
+              "https://chimpstatic.com/mcjs-connected/js/users/0f3bdc042292e4cb14add00a5/6dd4a0cf1015313748cf7f1c0.js"
+            );
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
