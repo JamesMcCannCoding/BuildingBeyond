a535e06d02d047e0b3b87d658637a8ad8f898883
James McCann <JMcCann@cfmeu.org>
2026-09-04 15:48:41 +1000

    Added hidden registration page

--- components/register-modal.tsx
+++ components/register-modal.tsx
@@ -180,8 +180,7 @@ export default function RegisterInterestModal({
     <div className={styles.successMessage}>
       <h3>Thank you</h3>
       <p>
-        You will recieve a follow up email from Bulding Beyond 2032. 
-        <br>If you can't see our email in your inbox, please check your spam, junk or promotional folders.</br>
+        You will recieve a follow up email from Bulding Beyond 2032. If you can't see our email in your inbox, please check your spam, junk or promotional folders.
       </p>
 
       {variant === "modal" && (
